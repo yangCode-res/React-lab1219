f762cfc57f1f7336399715d5c96acd51fa4a034c
Countsheep <1378055043@qq.com>
2024-12-02 22:47:59 +0800

    new update

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,22 +1,25 @@
 import './Home.css'
 const Home: React.FC = () => {
-    return (<div className="mainContainer">
-      <div className='postContainer'>
-        <div className='imageBox'>
-        <img src='../src/assets/kangyan.svg' >
+  return (<div className="mainContainer">
+    <div className='postContainer'>
+      <div className='leftBox'>
+      <div className='imageBox'>
+        <img src='../src/assets/kangyan.svg'  width={100} height={150}>
         </img>
+      </div>
+      <span className='SpanLeft'> 导师：康雁</span>
+      <span className='SpanLeft'> 联系方式:1378055043@qq.com</span>
+      </div>
+     
+      
        
-        </div>
-        <div>
-          <span> 导师：康雁</span>
-        </div>
-      </div>
-      <div className='secPostContainer'>
-        this is second
-      </div>
+    
+    </div>
+    <div className='secPostContainer'>
+      this is second
+    </div>
 
-    </div>);
-  };
-  
+  </div>);
+};
+
 export default Home;
-  
